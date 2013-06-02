28dd5c75e4ae613dc5045ba43118fa88facd9af9
btanaka <btanaka@crunchbang2012.(none)>
2013-06-01 18:53:31 -0700

    Messing around with sphinx themes

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -13,6 +13,10 @@
 
 import sys, os
 
+# for bootstrap theme - bt
+# see http://ryan-roemer.github.io/sphinx-bootstrap-theme/README.html#introduction-and-demos
+import sphinx_bootstrap_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -95,6 +99,13 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'default'
+#html_theme = 'agogo'
+#html_theme = 'scrolls'
+#html_theme = 'haiku'
+
+# bootstrap theme - http://ryan-roemer.github.io/sphinx-bootstrap-theme/README.html#introduction-and-demos - bt
+#html_theme = 'bootstrap'
+#html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
